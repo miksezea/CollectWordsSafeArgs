f6dc767c39c7e01e1cd00815ebd724da48c4ea73
miksezea <mikkel.eilskov@gmail.com>
2023-09-12 15:10:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/collectwordssafeargs/FirstFragment.kt
@@ -0,0 +1,61 @@
+package com.example.collectwordssafeargs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.collectwordssafeargs.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private var words = ArrayList<String>()
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSave.setOnClickListener {
+            val word = binding.editTextWord.text.toString().trim()
+            if (word.isEmpty()) {
+                binding.editTextWord.error = "No word"
+                return@setOnClickListener
+            }
+            words.add(word)
+            binding.textViewMessage.text = word + " was added to the list"
+        }
+
+        binding.buttonShow.setOnClickListener {
+            binding.textViewFirstList.text = words.toString()
+        }
+
+        binding.buttonShowNext.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(words.toString(), words)
+            findNavController().navigate(action)
+        }
+
+        binding.buttonClear.setOnClickListener {
+            words = ArrayList()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
